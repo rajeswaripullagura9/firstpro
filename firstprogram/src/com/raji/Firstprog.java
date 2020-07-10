c758ffd65f9de1ed92a8f1f6ac873adad33bfd0e
rajeswaripullagura <rajipullagura99@gmail.com>
2020-07-10 17:05:46 +0530

    sample program

---
+++ firstprogram/src/com/raji/Firstprog.java
@@ -0,0 +1,18 @@
+package com.raji;
+
+public class Firstprog {
+int a=9;
+int b=8;
+
+public void show() {
+	int a=3;
+	int b=44;
+	System.out.println(a+b);
+}
+
+
+public static void main(String[] args) {
+	Firstprog firstprog = new Firstprog();
+	System.out.println(firstprog.a);
+}
+}
